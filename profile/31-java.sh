f871b7251353085a1dd8dfbda17d9b9268ac39cb
Ned Rihine <ned.rihine@gmail.com>
2019-06-22 21:38:18 +0900

    sdkman fix

--- profile/31-java.sh
+++ profile/31-java.sh
@@ -3,6 +3,6 @@
 if [ -d $HOME/.sdkman ]; then
     export SDKMAN_DIR=$HOME/.sdkman
 
-    [[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && $SDKMAN_DIR/bin/sdkman-init.sh
+    [[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source $SDKMAN_DIR/bin/sdkman-init.sh
 fi
 
