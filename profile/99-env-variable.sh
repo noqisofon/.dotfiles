48d20b335a7c05dd1e328989d00e958d5a656a90
Ned Rihine <ned.rihine@gmail.com>
2020-12-17 10:17:45 +0900

    .zsh.d とか見て追記

--- profile/99-env-variable.sh
+++ profile/99-env-variable.sh
@@ -1,6 +1,43 @@
 # -*- coding: utf-8-unix; -*-
 
+test -z "$LANG" && export LANG=ja_JP.utf8
+
+export KCODE=u
+
 export EDITOR=vim
 export VISUAL=vim
 
-export PATH=$PATH:/opt/bin:$HOME/bin
+if ! type vim > /dev/null 2>&1; then
+    alias vim=vi
+fi
+
+export LISTMAX=0
+
+export LESS='-R'
+
+if [ -f $HOME/.source-highlight/src-hilite-lesspipe.sh ]; then
+    export LESSOPEN="| $HOME/.source-highlight/src-hilite-lesspipe.sh %s"
+fi
+
+if [ -z "$TERM" -o "$TERM" = "unknown" ]; then
+    TERM=linux
+fi
+
+if [ "$TERM" = "linux" ]; then
+    export LC_TIME=C
+    export LC_MESSAGES=C
+fi
+
+if is-windows-nt; then
+    export XDG_CONFIG_HOME=$HOME/.config
+    export XDG_DATA_HOME=$HOME/.local/share
+    export XDG_CACHE_HOME=$HOME/.cache
+fi
+
+export HISTFILESIZE=100000
+
+if [ -z "$SCOOP" ]; then
+    export PATH=$PATH:/opt/bin:/usr/share/perl6/site/bin:$HOME/bin:$HOME/.local/bin:$HOME/.roswell/bin
+else
+    export PATH=$PATH:/opt/bin:/usr/share/perl6/site/bin:$HOME/bin:$HOME/.local/bin:$HOME/.roswell/bin:$SCOOP/shims
+fi
