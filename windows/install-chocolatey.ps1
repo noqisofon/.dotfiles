1fd040c107c1c97e2e88b54f89862fd20619b3d0
Ned Rihine <ned.rihine@gmail.com>
2021-07-10 13:20:40 +0900

    .bat を .ps1 にしたり、先頭のどっとを消したりした

--- windows/install-chocolatey.ps1
+++ windows/install-chocolatey.ps1
@@ -1,13 +1,11 @@
-@echo off
-
-rem 
-rem install-windows-ny.bat
-rem
-rem Chocolatey をインストールし、Chocolatey でインストールしていたものをインストールします。
-rem 
+#
+# install-chocolatey.ps1
+#
+# Chocolatey をインストールし、そのとき Chocolatey でインストールしていたものをインストールします。
+# 
 
 
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 
 
@@ -29,6 +27,6 @@ choco install SourceCodePro -y
 choco install vcredist2010 -y
 choco install vlc -y
 choco install winamp -y
-choco install you-get -y
+# choco install you-get -y
 choco install youtube-dl -y
 choco install firefox -y
