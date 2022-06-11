11c6284c2ea6330fe08ad2b8cf942825c70d5784
Ned Rihine <ned.rihine@gmail.com>
2022-06-11 23:26:21 +0900

    Scoop で rakudo-moar をインストールしているので必要ないかも

--- profile/34-raku.sh
+++ profile/34-raku.sh
@@ -1,27 +1,27 @@
 # -*- coding: utf-8-unix; -*-
 
-RAKUENV_BIN_DIR=$HOME/.rakuenv/bin
+# RAKUENV_BIN_DIR=$HOME/.rakuenv/bin
 
-if [ -d "RAKUENV_BIN_DIR" ]; then
-    export RAKUENV_BIN_DIR
+# if [ -d "RAKUENV_BIN_DIR" ]; then
+#     export RAKUENV_BIN_DIR
 
-    PATH=$PATH:$RAKUENV_BIN_DIR
-fi
+#     PATH=$PATH:$RAKUENV_BIN_DIR
+# fi
 
-if is-windows-nt ; then
-    WINDOWS_RAKU_BIN_DIR=/c/rakudo/bin
+# if is-windows-nt ; then
+#     WINDOWS_RAKU_BIN_DIR=/c/rakudo/bin
 
-    if [ -d "$WINDOWS_RAKU_BIN_DIR" ]; then
-	export WINDOWS_RAKU_BIN_DIR
+#     if [ -d "$WINDOWS_RAKU_BIN_DIR" ]; then
+# 	export WINDOWS_RAKU_BIN_DIR
 
-	PATH=$PATH:$WINDOWS_RAKU_BIN_DIR
-    fi
-fi
+# 	PATH=$PATH:$WINDOWS_RAKU_BIN_DIR
+#     fi
+# fi
 
-RAKU_BIN_DIR=/usr/share/perl6/site/bin
+# RAKU_BIN_DIR=/usr/share/perl6/site/bin
 
-if [ -d "$RAKU_BIN_DIR" ]; then
-    export RAKU_BIN_DIR
+# if [ -d "$RAKU_BIN_DIR" ]; then
+#     export RAKU_BIN_DIR
 
-    PATH=$PATH:$RAKU_BIN_DIR
-fi
+#     PATH=$PATH:$RAKU_BIN_DIR
+# fi
