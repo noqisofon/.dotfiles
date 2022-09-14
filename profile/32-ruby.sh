3ceb9e7cfceda5da6590b80d0898f97712761b3c
Ned Rihine <ned.rihine@gmail.com>
2022-09-14 23:01:08 +0900

    cut の使い方がわかった

--- profile/32-ruby.sh
+++ profile/32-ruby.sh
@@ -1,17 +1,13 @@
 # -*- coding: utf-8-unix; -*-
 
 ruby-version() {
-    ruby -e 'print RUBY_VERSION'
+    local ruby_version=$(ruby -e 'puts RUBY_VERSION')
+    local ruby_major_version=$(echo $ruby_version | cut -d. -f1)
+    local ruby_minor_version=$(echo $ruby_version | cut -d. -f2)
+
+    echo "${ruby_major_version}.${ruby_minor_version}.0"
 }
 
-if [ "$MSYSTEM" = 'MSYS' ]; then
-    ruby_version=2.7.0
-else
-    ruby_version=3.0.0
-fi
-
-export GEM_HOME=$HOME/.gem/ruby/${ruby_version}
+export GEM_HOME=$HOME/.gem/ruby/$(ruby_version)
 
 export gem_bin_dir=$GEM_HOME/bin
-
-unset ruby_version
--- profile/33-perl.sh
+++ profile/33-perl.sh
@@ -1,7 +1,11 @@
 # -*- coding: utf-8-unix; -*-
 
 perl-version() {
-    perl -e 'print $^V'
+    local perl_version=$(perl -e 'print $^V' | sed s/v//)
+    local perl_major_version=$(echo $perl_version | cut -d. -f1)
+    local perl_minor_version=$(echo $perl_version | cut -d. -f2)
+
+    echo "${perl_major_version}.${perl_minor_version}.0"
 }
 
-export PERL5LIB=$HOME/.perl5/lib/5.32
+export PERL5LIB=$HOME/.perl5/lib/$(perl-version)
